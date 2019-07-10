2bcd5068fb2ba5fd17d45c56af399047c5baee9e
dwyer840 <jacob.dwyer840@gmail.com>
2019-07-10 02:33:24 -0700

    My bit coind price tracker. love this and felt I learned some important things from here. thank you. all the comments are for future me for reference

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,16 +12,23 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.Header;
 
-
+//BEFORE LOOKING AT THIS AND GETTING CONFUSED NOTE THAT THERE IS ALSO A WEATHER APP WE MADE
+//LOOK AT THE DIFFERENCES BETWEEN THE TWO.
+//IN WEATHER APP WE HAD A WHOLE CLASS TO HANDLE JSON STUFF AND IN THIS CLASSS WE CREATED A SPECIALL FROM JSON CONSTRUCTOR... BASICALLY AND WE USED THE OTHER CLASS
+// IE THE WEATHER CONTROLLER APP
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
 
     // Member Variables:
     TextView mPriceTextView;
@@ -30,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //**BELOW IS AN EXCELLENT EXAMPLE OF HOW TO CRANK OUT A DROP DOWN MENU TYPE THING OR
+        //IE THE SPINNER (MAY BE OTHER DROP DOWN THINGS... NOT TOO SURE
         mPriceTextView = (TextView) findViewById(R.id.priceLabel);
         Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
 
@@ -45,35 +53,61 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {//parent, view, position, rowID?
+                adapterView.getItemAtPosition(i); // see the google documentation https://developer.android.com/guide/topics/ui/controls/spinner.html
+                                                    //AdapterView is the parent... all i can say now...
+                Log.d("Bitcoin", "" + adapterView.getItemAtPosition(i));
 
+                letsDoSomeNetworking(BASE_URL+adapterView.getItemAtPosition(i));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Log.d("Bitcoin", "Nothing Selected");
+            }
+        });
     }
 
     // TODO: complete the letsDoSomeNetworking() method
     private void letsDoSomeNetworking(String url) {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url, new JsonHttpResponseHandler(){
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                Log.d("Bitcoin", "JSON: " + response.toString());
+
+                //we need try catch for json. response gives us all the crap we got from bitcoin json object!
+
+                try {
+                    String ask = response.getString("ask");
+
+                    mPriceTextView.setText(ask);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d("Bitcoin", "Request fail! Status code: " + statusCode);
+                Log.d("Bitcoin", "Fail response: " + response);
+                Log.e("ERROR", e.toString());
+
+            }
+        });
 
 
     }
+
+
 
 
 }
